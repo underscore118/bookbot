534e2aab91a2932a0c665c4f0c43bd28e10fdb30
underscore118 <vittorio.giordana@protonmail.com>
2025-09-05 20:37:50 +0200

    Report

--- main.py
+++ main.py
@@ -2,23 +2,47 @@ def main():
     
     from stats import get_number_words
     from stats import get_chars_dict
-
+    from stats import get_report
 
     book_path = "books/frankenstein.txt"
     text_content = get_book_text(book_path)
     Nletter = {}
+    report = {}
 
 
-    Nword = f"{get_number_words(text_content)} words found in the document"
+    Nword = f"{get_number_words(text_content)}"
     Nletter = get_chars_dict(text_content)
+    report = get_report(Nletter)
     
-    print(Nletter)
-    print(Nword)
+    print_repo(book_path, Nword, report)
+
+
+    #print(report)
+    #print(Nletter)
+    #print(Nword)
 
 
 def get_book_text(path_file):
     with open(path_file) as f:
        return f.read()
+    
+def print_repo(book_path, Nwords, report):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at "+ book_path)
+    print("----------- Word Count ----------")
+    print("Found " + Nwords + " total words")
+    print("--------- Character Count -------")
+
+
+    for item in report:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]} ")
+
+
+    print("============= END ===============")
+    
+ 
+    return None
 
 
 
--- stats.py
+++ stats.py
@@ -1,8 +1,10 @@
+#calcola il numero di parole
 def get_number_words(text):
     words = []
     words = text.split()
     return len(words)
 
+#conta quante volte appaiono ogni carattere nel testo
 def get_chars_dict(text):
     char = {}
     for c in text:
@@ -14,3 +16,16 @@ def get_chars_dict(text):
     return char
 
 
+#crea il report
+def get_report(charCount):
+    splitChart = []
+    for char, num in charCount.items():
+        splitChart.append({"char" : char, "num" : num})  
+
+    splitChart.sort(key=sort_on, reverse= True)
+
+    return splitChart
+
+
+def sort_on(item):
+    return item["num"]
